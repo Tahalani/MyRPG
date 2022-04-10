cdd9b5c0e5ecb01691a257c2f2b38d8aece88df3
Taha ALANI <taha.alani@epitech.eu>
2022-04-11 01:50:45 +0200

    [ADD] pnj avec leur position et bientot les premiers dialogues

--- src/pnj/position_pnj.c
+++ src/pnj/position_pnj.c
@@ -13,9 +13,8 @@ int set_position_pnj(game_t *game)
     game->pnj[0].position = (sfVector2f){2500, 1300};
     game->pnj[1].position = (sfVector2f){1280, 520};
     game->pnj[2].position = (sfVector2f){700, -170};
-    sfSprite_setPosition(game->pnj[0].sprite, game->pnj[0].position);
-    sfSprite_setPosition(game->pnj[1].sprite, game->pnj[1].position);
-    sfSprite_setPosition(game->pnj[2].sprite, game->pnj[2].position);
+    for (int i = 0; i != 3; i++)
+        sfSprite_setPosition(game->pnj[i].sprite, game->pnj[i].position);
     set_rect_pnj(game);
     return (0);
 }
--- src/pnj/rect_pnj.c
+++ src/pnj/rect_pnj.c
@@ -13,7 +13,6 @@ int set_rect_pnj(game_t *game)
     game->pnj[0].rect = (sfIntRect){31.3333333, 0, 31, 48};
     game->pnj[1].rect = (sfIntRect){31.6666667, 48, 31, 48};
     game->pnj[2].rect = (sfIntRect){48.3333333, 0, 48, 48};
-    for (int i = 0; i != 3; i++) {
+    for (int i = 0; i != 3; i++)
         sfSprite_setTextureRect(game->pnj[i].sprite, game->pnj[i].rect);
-    }
 }
