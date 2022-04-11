f62a50dc640a99891631775c70a2b644a873fc65
Taha ALANI <taha.alani@epitech.eu>
2022-04-11 03:02:35 +0200

    [EDIT] docing style

--- src/pnj/display.c
+++ src/pnj/display.c
@@ -11,7 +11,8 @@
 int display_pnj(game_t *game)
 {
     for (int i = 0; i != 3; i++) {
-        sfRenderWindow_drawSprite(game->window.window, game->pnj[i].sprite, NULL);
+        sfRenderWindow_drawSprite
+        (game->window.window, game->pnj[i].sprite, NULL);
     }
     return (0);
 }
