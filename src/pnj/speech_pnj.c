0e5b0d8462c61c1ac8e53144a882ee3adf05b45f
Taha ALANI <taha.alani@epitech.eu>
2022-04-19 16:59:13 +0200

    [ADD] sound effect

--- src/pnj/speech_pnj.c
+++ src/pnj/speech_pnj.c
@@ -10,20 +10,18 @@
 
 int condition_display_speech(game_t *game)
 {
-    if (game->map->x_player == 22 && game->map->y_player == 19) {
-        sfMusic_play(game->sound.dialogue);
+    if (game->map->x_player == 22 && game->map->y_player == 19)
         sfRenderWindow_drawSprite
         (game->window.window, game->speech[0].sprite, NULL);
-    }
-    if (game->map->x_player == 30 && game->map->y_player == 30) {
-        sfMusic_play(game->sound.dialogue);
+    if (game->map->x_player == 30 && game->map->y_player == 30)
         sfRenderWindow_drawSprite
         (game->window.window, game->speech[1].sprite, NULL);
-    }
-    if (game->map->x_player == 54 && game->map->y_player == 45) {
-        sfMusic_play(game->sound.dialogue);
+    if (game->map->x_player == 54 && game->map->y_player == 45)
         sfRenderWindow_drawSprite
         (game->window.window, game->speech[2].sprite, NULL);
-    }
+    if ((game->map->x_player == 22 && game->map->y_player == 20) ||
+    (game->map->x_player == 30 && game->map->y_player == 31) || 
+    (game->map->x_player == 54 && game->map->y_player == 46))
+        sfMusic_play(game->sound.dialogue);
     return (0);
 }
