a6307ffbf1e11de4dc690fef8803ae167a5493a7
Taha ALANI <taha.alani@epitech.eu>
2022-04-22 03:15:46 +0200

    [ADD] dialogue and story boarding started

--- src/inventory/loop_inventory.c
+++ src/inventory/loop_inventory.c
@@ -8,11 +8,11 @@
 #include "my.h"
 #include "rpg.h"
 
-
-
 int inventory_loop(game_t *game)
 {
     display_natural_object(game);
+    if (game->map[1].check == 1 && game->map[1].x_player == 28 && game->map[1].y_player == 7)
+        game->story_steps = 3;
     move_rect(&game->inventory.object[0].rect, 480, 3840, 0);
     sfSprite_setTextureRect(game->inventory.object[0].sprite,
     game->inventory.object[0].rect);
